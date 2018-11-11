6a887c9ba6949f945d45c3861739ff4f72cfdcd6
fadilnatakusumah <fadil.ntksmh@gmail.com>
2018-11-11 22:28:08 +0700

    fixing database connection

--- server.js
+++ server.js
@@ -21,10 +21,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : '12345',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
